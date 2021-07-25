ce516accd3ac2674bda3d93d8ae40ba9765e96f3
nickbron <77615583+nickbron@users.noreply.github.com>
2021-07-25 17:25:03 +0300

    create

--- src/index.js
+++ src/index.js
@@ -1,1 +1,75 @@
-import './sass/main.scss';
+import './css/styles.css';
+import API from './js/fetchPictures';
+import { Notify } from "notiflix";
+//import Notiflix from 'notiflix';
+import cardsTpl from './templates/cards.hbs';
+import SimpleLightbox from "simplelightbox";
+
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  cards: document.querySelector('.gallery'),
+  buttonLoadMore: document.querySelector('.load-more'),
+};
+
+refs.buttonLoadMore.addEventListener("click", OnMore);
+refs.searchForm.addEventListener('submit', onSearch);
+let page = 1;
+let name;
+  
+async function onSearch(e) {
+  e.preventDefault();
+  name = e.currentTarget.elements.searchQuery.value;
+  if (name.trim() === "") { Notify.failure("Sorry, there are no images matching your search query. Please try again."); return;}
+  try {
+    const cards = await API.fetchPictures(name, page);
+     clearimagesContainer();
+    if (cards.data.total > 0) {
+      refs.buttonLoadMore.classList.remove('is-hidden');
+      renderCardsimages(cards);
+       Notify.info(`Hooray! We found ${cards.data.total} images.`);
+    }
+    else
+    {
+     Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+      }
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+function renderCardsimages(cards) {
+  const markup = cardsTpl(cards);
+  refs.cards.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearimagesContainer() {
+  refs.cards.innerHTML = '';
+}
+
+async function OnMore() {
+  const cards = await API.fetchPictures(name, ++page);
+  renderCardsimages(cards);
+  Notify.info(`Hooray! We found ${cards.data.total} images.`);
+
+  //Box();
+
+}
+//---------------------------------------------------
+
+
+
+// 1 - fetch картинки
+// 2 - рисуем
+// 3 - рефреш либы
+
+async function Box()
+{
+  let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// do something…
+  console.log("eddeded");
+});
+
+}
---
+++ src/js/fetchPictures.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+const API_KEY = '22624023-cde2aba775d295bec8b1b2952';
+const BASE_URL = 'https://pixabay.com/api/';
+const params = 'image_type=photo$orientation=horizontal&safesearch=true&per_page=40';
+
+async function fetchPictures(name,page) {
+    
+    try {
+        const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${name}&${params}&page=${page}`);
+    return response;
+    }
+    catch (error) {
+    console.error(error);
+    }
+}
+
+export default {fetchPictures};
+
+
